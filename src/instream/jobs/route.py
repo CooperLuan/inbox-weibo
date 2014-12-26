14bd295f4c5b7a6ab83cba6cbdafbabd6ba2fa59
Cooper.luan <gc.suprs@gmail.com>
2014-12-26 22:14:40 +0800

    to be continued

--- src/instream/jobs/route.py
+++ src/instream/jobs/route.py
@@ -1,5 +1,19 @@
+import importlib
+
 from .job import Job
+from instream import env
 
 
 class Route(Job):
-    pass
+
+    def run(self, route, _id, collection):
+        """
+        @route str : crawler.weibo.statuses
+        """
+        doc = env.MONGO[collection].find_one({'_id': _id})
+        kwargs = doc.pop('kwargs', {})
+        module_path = 'instream.jobs.' + route + '_job'
+        route_parts = route.split('.')
+        module = importlib.import_module(module_path)
+        job_class = getattr(module, module.__all__[0])
+        job_class().apply_async(kwargs=kwargs, queue=route_parts[0])
--- src/instream/jobs/scheduler/scheduler.py
+++ src/instream/jobs/scheduler/scheduler.py
@@ -23,6 +23,17 @@ class Scheduler(Job):
         }
         if doc['title'] in self.collection.distinct('title'):
             raise Exception('Existed schedule title %s' % doc['title'])
-        self.collection.insert(doc)
-        # add task
-        Route
+        _id = self.collection.insert(doc)
+        doc_seed = {
+            "_scheduleId": _id,
+            "_status": None,
+            "_seedId": None,
+            "_ancestors": [],
+            "route": doc['route'],
+            "incremental": doc['incremental'],
+            "created": time.time(),
+            "updated": time.time(),
+            "kwargs": doc['kwargs']
+        }
+        _id = env.MONGO.seeds.insert(doc_seed)
+        Route().run(doc['route'], _id, 'seeds')
